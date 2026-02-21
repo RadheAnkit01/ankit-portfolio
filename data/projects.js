fb19e60c0ef48f4180e4aa597e86e0e01a841411
Ankit Raj <radhe@Radhes-MacBook-Air.local>
2026-02-21 21:52:38 +0530

    changed some details and structure

--- data/projects.js
+++ data/projects.js
@@ -1,29 +1,29 @@
 export const projects = [
   
   {
-  id: "Wallpaper App - Flutter",
-  title: "Wallpaper App using Pexels API",
-  shortDescription:
-    "A cross-platform wallpaper application built with Flutter using the Pexels Photo API.",
-  description:
-    "A Flutter-based wallpaper application that fetches high-quality images from the Pexels API. The app supports downloading wallpapers on both platforms, with full wallpaper setting functionality on Android (Home Screen, Lock Screen, or both). On iOS, due to system restrictions, users can download wallpapers only. The project focuses on API integration, platform-specific permissions, and real-device testing.",
-  tech: ["Flutter", "Dart", "Pexels API"],
-  images: [
-    "assets/images/wallpaper flutter/home.png",
-    "assets/images/wallpaper flutter/fullscreen.png",
-    "assets/images/wallpaper flutter/options.png",
-  ],
-  github: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git",
-  live: "#",
-  buttons: [
-    {
-      label: "GitHub",
-      link: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git"
-    },
-    // {
-    //   label: "Live Demo",
-    //   link: "#"
-    // }
-  ]
-}
+    id: "Wallpaper App - Flutter",
+    title: "Wallpaper App using Pexels API",
+    shortDescription:
+      "A cross-platform wallpaper application built with Flutter using the Pexels Photo API.",
+    description:
+      "A Flutter-based wallpaper application that fetches high-quality images from the Pexels API. The app supports downloading wallpapers on both platforms, with full wallpaper setting functionality on Android (Home Screen, Lock Screen, or both). On iOS, due to system restrictions, users can download wallpapers only. The project focuses on API integration, platform-specific permissions, and real-device testing.",
+    tech: ["Flutter", "Dart", "Pexels API"],
+    images: [
+      "assets/images/wallpaper flutter/home.png",
+      "assets/images/wallpaper flutter/fullscreen.png",
+      "assets/images/wallpaper flutter/options.png",
+    ],
+    github: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git",
+    live: "#",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git"
+      },
+      // {
+      //   label: "Live Demo",
+      //   link: "#"
+      // }
+    ]
+  },
 ];
