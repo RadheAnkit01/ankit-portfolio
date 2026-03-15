db155a3060c7379fda59b3830e51536cf5e14efa
Ankit Raj <radheankit110031@gmail.com>
2026-03-15 23:56:34 +0530

    project whatsapp clone added

--- data/projects.js
+++ data/projects.js
@@ -13,17 +13,48 @@ export const projects = [
       "assets/images/wallpaper flutter/fullscreen.png",
       "assets/images/wallpaper flutter/options.png",
     ],
-    github: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git",
+    github: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter",
     live: "#",
     buttons: [
       {
         label: "GitHub",
-        link: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter.git"
+        link: "https://github.com/RadheAnkit01/Wallpaper-App-Flutter"
       },
       // {
       //   label: "Live Demo",
       //   link: "#"
       // }
+    ]
+  },
+  {
+    id: "whatsapp-clone-flutter",
+    title: "WhatsApp Clone UI - Flutter",
+    shortDescription: "A Flutter project that recreates the WhatsApp interface to demonstrate mobile UI development and multi-screen navigation.",
+
+    description: "A WhatsApp-inspired mobile application built using Flutter to practice real-world UI development and application layout design. The project recreates the core WhatsApp interface including multiple screens such as Splash, Welcome, Phone Number Entry, OTP Verification, Home, Chats, Status, Calls, and Profile Info. The focus of this project was to strengthen Flutter fundamentals such as widget composition, responsive layouts, and screen navigation. The entire codebase was written from scratch without relying on external tutorials.",
+
+    tech: ["Flutter", "Dart", "Material UI"],
+    images: [
+      "assets/images/whatsapp/whatsapp1.png",
+      "assets/images/whatsapp/whatsapp2.png",
+      "assets/images/whatsapp/whatsapp3.png",
+      "assets/images/whatsapp/whatsapp4.png",
+      "assets/images/whatsapp/whatsapp5.png",
+      "assets/images/whatsapp/whatsapp6.png",
+      "assets/images/whatsapp/whatsapp7.png",
+      "assets/images/whatsapp/whatsapp8.png",      
+    ],
+    github: "https://github.com/RadheAnkit01/Whatsapp-Clone",
+    live: "https://drive.google.com/file/d/1F_NvNTtQ7IlN7z-mrkFKKLvnWSTrJ7ub/view?usp=sharing",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/RadheAnkit01/Whatsapp-Clone"
+      },
+      {
+        label: "Download Apk",
+        link: "https://drive.google.com/file/d/1F_NvNTtQ7IlN7z-mrkFKKLvnWSTrJ7ub/view?usp=sharing"
+      }
     ]
   },
 ];
